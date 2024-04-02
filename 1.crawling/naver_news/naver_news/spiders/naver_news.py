a3e05e3a9da501f4b2d59da056b764054f9198d0
sujung <qortnwjd58@naver.com>
2024-04-02 16:43:53 +0900

    Fix: scrapy code

--- 1.crawling/naver_news/naver_news/spiders/naver_news.py
+++ 1.crawling/naver_news/naver_news/spiders/naver_news.py
@@ -1,14 +1,20 @@
+from naver_news.items import NaverNewsItem
 import scrapy
 from bs4 import BeautifulSoup
 import json
+# from evn_keyword_list import evn_keyword
 
 class NaverNewsSpider(scrapy.Spider):
     name = "naver_news"
     
     def start_requests(self):
+        # companys = company_list
+        # keywords = evn_keyword + social_keyword + gvn_keyword
+        # for compny in companys :
         keywords = ['삼성']
         for keyword in keywords:
-            for url_num in range(1,11,10):
+            for url_num in range(1,11,10):  # 10의 배수로 갯수 지정
+                # keyword = f'{compny} {keyword}'
                 url_num = str(url_num)
                 jq_url = f'https://s.search.naver.com/p/newssearch/search.naver?cluster_rank={url_num}&de=&ds=&eid=&field=0&force_original=&is_dts=0&is_sug_officeid=0&mynews=0&news_office_checked=&\
                     nlu_query=&nqx_theme=%7B%22theme%22%3A%7B%22main%22%3A%7B%22name%22%3A%22site%22%2C%22score%22%3A%220.831540%22%7D%7D%7D&nso=%26nso%3Dso%3Ar%2Cp%3Aall%2Ca%3Aall&nx_and_query=\
@@ -30,12 +36,14 @@ class NaverNewsSpider(scrapy.Spider):
             for link in links:
                 href = link['href']
                 if 'https://n.news.naver.com' in href:
-                    print(href)
                     yield scrapy.Request(url=href, callback=self.parse, headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'})
 
     def parse(self, response):
+        date = response.css('.media_end_head_info_datestamp_bunch span::text')
+        date = date[0].get().split(' ')[0]
+
         # br태크이므로 article태그안의 내용을 다가져옴
         selectors = response.css('article::text')
-        # selectors로 이뤄진 리스트 구조 for문을 돌려 텍스트만 추출 햇음
         contents = [selector.get().strip() for selector in selectors if selector.get().strip()]
-        
+
+        print(date, contents)
