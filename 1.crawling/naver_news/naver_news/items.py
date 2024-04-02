4db26758c7f8523af33fc9815174645520020751
sujung <qortnwjd58@naver.com>
2024-04-02 16:45:54 +0900

    fix : scrapy change

--- 1.crawling/naver_news/naver_news/items.py
+++ 1.crawling/naver_news/naver_news/items.py
@@ -7,6 +7,5 @@ import scrapy
 
 
 class NaverNewsItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    date = scrapy.Field()
+    content = scrapy.Field()
